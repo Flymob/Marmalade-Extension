817f3ddedba4350ac01608147718e53ccc50034a
Aleksey Baskakov <a.baskakov@macmini-baskakov.it.lan>
2016-10-26 13:12:28 +0300

    android implementation

--- source/android/FlyMob.java
+++ source/android/FlyMob.java
@@ -9,74 +9,173 @@ These functions are called via JNI from native code.
  * NOTE: This file was originally written by the extension builder, but will not
  * be overwritten (unless --force is specified) and is intended to be modified.
  */
+
 import com.ideaworks3d.marmalade.LoaderAPI;
+
+import com.flymob.sdk.common.ads.FailResponse;
+import com.flymob.sdk.common.ads.interstitial.FlyMobInterstitial;
+import com.flymob.sdk.common.ads.interstitial.IFlyMobInterstitialListener;
+import com.flymob.sdk.common.ads.video.FlyMobRewardedVideo;
+import com.flymob.sdk.common.ads.video.IFlyMobRewardedVideoListener;
+
+import com.ideaworks3d.marmalade.LoaderAPI;
+import com.ideaworks3d.marmalade.LoaderActivity;
 // Comment in the following line if you want to use ResourceUtility
 // import com.ideaworks3d.marmalade.ResourceUtility;
 
-class FlyMob
-{
-    public void s3eFlyMobInterstitialInitialize(FIXME zoneID)
-    {
-        
+class FlyMob {
+    FlyMobInterstitial mFlyMobInterstitial;
+    FlyMobRewardedVideo mFlyMobRewardedVideo;
+
+    public void s3eFlyMobInterstitialInitialize(int zoneID) {
+        Context context = (Context) LoaderActivity.m_Activity;
+        if (myContext != null) {
+            mFlyMobInterstitial = new FlyMobInterstitial(context, zoneID);
+            mFlyMobInterstitial.addListener(new IFlyMobInterstitialListener() {
+                @Override
+                public void loaded(FlyMobInterstitial interstitial) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_INTERSTITIAL_DID_LOAD);
+                }
+
+                @Override
+                public void failed(FlyMobInterstitial interstitial, FailResponse response) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_INTERSTITIAL_DID_FAIL);
+                }
+
+                @Override
+                public void shown(FlyMobInterstitial interstitial) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_INTERSTITIAL_DID_SHOW);
+                }
+
+                @Override
+                public void clicked(FlyMobInterstitial interstitial) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_INTERSTITIAL_DID_CLICK);
+                }
+
+                @Override
+                public void closed(FlyMobInterstitial interstitial) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_INTERSTITIAL_DID_CLOSE);
+                }
+
+                @Override
+                public void expired(FlyMobInterstitial interstitial) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_INTERSTITIAL_DID_EXPIRE);
+                }
+            });
+        }
     }
-    public void s3eFlyMobInterstitialLoad()
-    {
-        
+
+    public void s3eFlyMobInterstitialLoad() {
+        if (mFlyMobInterstitial != null) {
+            mFlyMobInterstitial.load();
+        }
     }
-    public void s3eFlyMobInterstitialShow()
-    {
-        
+
+    public void s3eFlyMobInterstitialShow() {
+        if (mFlyMobInterstitial != null) {
+            mFlyMobInterstitial.show();
+        }
     }
-    public boolean s3eFlyMobInterstitialIsReady()
-    {
+
+    public boolean s3eFlyMobInterstitialIsReady() {
+        if (mFlyMobInterstitial != null) {
+            return mFlyMobInterstitial.isLoaded();
+        }
         return false;
     }
-    public void s3eFlyMobRewardedVideoInitialize(FIXME zoneID)
-    {
-        
+
+    public void s3eFlyMobRewardedVideoInitialize(int zoneID) {
+        Context context = (Context) LoaderActivity.m_Activity;
+        if (myContext != null) {
+            mFlyMobRewardedVideo = new FlyMobRewardedVideo(context, zoneID);
+
+            mFlyMobRewardedVideo.addListener(new IFlyMobRewardedVideoListener() {
+                @Override
+                public void loaded(FlyMobRewardedVideo video) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_REWARDED_VIDEO_DID_LOAD);
+                }
+
+                @Override
+                public void failed(FlyMobRewardedVideo video, FailResponse response) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_REWARDED_VIDEO_DID_FAIL);
+                }
+
+                @Override
+                public void shown(FlyMobRewardedVideo video) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_REWARDED_VIDEO_DID_SHOW);
+                }
+
+                @Override
+                public void closed(FlyMobRewardedVideo video) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_REWARDED_VIDEO_DID_CLOSE);
+                }
+
+                @Override
+                public void started(FlyMobRewardedVideo video) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_REWARDED_VIDEO_DID_START);
+                }
+
+                @Override
+                public void completed(FlyMobRewardedVideo video) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_REWARDED_VIDEO_DID_COMPLETE);
+                }
+
+                @Override
+                public void expired(FlyMobRewardedVideo video) {
+                    s3eEdkCallbacksEnqueue(S3E_EXT_FLYMOB_HASH, S3E_FLYMOB_REWARDED_VIDEO_DID_EXPIRE);
+                }
+            });
+        }
     }
-    public void s3eFlyMobRewardedVideoLoad()
-    {
-        
+
+    public void s3eFlyMobRewardedVideoLoad() {
+        if (mFlyMobRewardedVideo != null) {
+            mFlyMobRewardedVideo.load();
+        }
     }
-    public void s3eFlyMobRewardedVideoShow()
-    {
-        
+
+    public void s3eFlyMobRewardedVideoShow() {
+        if (mFlyMobRewardedVideo != null) {
+            mFlyMobRewardedVideo.show();
+        }
     }
-    public boolean s3eFlyMobRewardedVideoIsReady()
-    {
+
+    public boolean s3eFlyMobRewardedVideoIsReady() {
+        if (mFlyMobRewardedVideo != null) {
+            return mFlyMobRewardedVideo.isLoaded();
+        }
         return false;
     }
-    public void s3eFlyMobSDKSetDebugMode(boolean isDebug)
-    {
-        
+
+    public void s3eFlyMobSDKSetDebugMode(boolean isDebug) {
+        com.flymob.sdk.common.FlyMob.setDebugMode(isDebug);
     }
-    public boolean s3eFlyMobSDKIsDebugMode()
-    {
-        return false;
+
+    public boolean s3eFlyMobSDKIsDebugMode() {
+        return com.flymob.sdk.common.FlyMob.isDebugMode();
     }
-    public void s3eFlyMobSDKSetCoppa(boolean isCoppa)
-    {
-        
+
+    public void s3eFlyMobSDKSetCoppa(boolean isCoppa) {
+        com.flymob.sdk.common.FlyMob.setCoppa(isCoppa);
     }
-    public boolean s3eFlyMobSDKIsCoppa()
-    {
-        return false;
+
+    public boolean s3eFlyMobSDKIsCoppa() {
+        return com.flymob.sdk.common.FlyMob.isCoppa();
     }
-    public void s3eFlyMobSDKSetDnt(boolean isDnt)
-    {
-        
+
+    public void s3eFlyMobSDKSetDnt(boolean isDnt) {
+        com.flymob.sdk.common.FlyMob.setDnt(isDnt);
     }
-    public boolean s3eFlyMobSDKIsDnt()
-    {
-        return false;
+
+    public boolean s3eFlyMobSDKIsDnt() {
+        return com.flymob.sdk.common.FlyMob.isDnt();
     }
-    public void s3eFlyMobSDKSetTesting(boolean isTesting)
-    {
-        
+
+    public void s3eFlyMobSDKSetTesting(boolean isTesting) {
+        com.flymob.sdk.common.FlyMob.setTesting(isTesting);
     }
-    public boolean s3eFlyMobSDKIsTesting()
-    {
-        return false;
+
+    public boolean s3eFlyMobSDKIsTesting() {
+        return com.flymob.sdk.common.FlyMob.isTesting();
     }
 }
